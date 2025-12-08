02f6d51204ee2a52150c71b193f0dd300cd06aa1
Vimtoooo <leonellivito444@gmail.com>
2025-12-08 15:37:45 -0300

    NEXT: Include a time formatter 24h or 12h

--- Digital_Clock.py
+++ Digital_Clock.py
@@ -3,7 +3,7 @@ from datetime import datetime as dt
 
 # Implement the Receiver class for executing particular commands
 class DigitalClock:
-    _instance: object = None
+    _instance = None
 
     def __new__(cls):
         if cls._instance is None:
@@ -11,12 +11,16 @@ class DigitalClock:
         return cls._instance
     
     def __init__(self, hours: int = 0, minutes: int = 0, seconds: int = 0):
+        if getattr(self, '_initialized', True):
+            return
+
+        self._hours: int | float = hours
+        self._minutes: int | float = minutes
+        self._seconds: int | float = seconds
         
-        if not hasattr(self, '_initialized'):
-            self._hours: int | float = hours
-            self._minutes: int | float = minutes
-            self._seconds: int | float = seconds
+        self._initialized: bool = True
         
+    # BUG: Define the clock's core methods...
     def set_time(self):
         now = dt.now()
 
@@ -24,7 +28,30 @@ class DigitalClock:
         self._minutes = now.minute
         self._seconds = now.second
     
+    def tick(self):
+        
+        if self._seconds + 1 >= 60:
+            self._seconds = 0
+
+            if self._minutes + 1 >= 60:
+                self._minutes = 0
+
+                if self._hours + 1 >= 24:
+                    self._hours = 0
+
+                else: 
+                    self._hours += 1
+
+            else: 
+                self._minutes += 1
+
+        else: 
+            self.seconds += 1
+    
+    def __str__(self) -> str:
+        return f""
 
 # Quick tests:
 myClock = DigitalClock()
 myClock.set_time()
+print(myClock._hours)
